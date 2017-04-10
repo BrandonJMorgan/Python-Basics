0e072245a1f88ab832a75ef86d7c2ebfd2c61891
brandonjayster <jayster@protonmail.com>
2017-04-10 02:56:56 -0500

    Added a while loop

--- Loop.py
+++ Loop.py
@@ -1,3 +1,15 @@
+#while loops
+con = 1
+while con < 10:
+    print (con)
+    con +=1
+
+# the result will be a count from 1-9
+
+
+
+
+
 exampleList = [1,2,4,77,89,101]
 for eachNumber in exampleList:
     print(eachNumber)
