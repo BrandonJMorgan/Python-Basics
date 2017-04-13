d5bb3f7a99751a26e6f25880b4d80eed7ebd2f63
Brandon <jayster@protonmail.com>
2017-04-12 22:36:51 -0500

    Update Full_lesson.py

--- Full_lesson.py
+++ Full_lesson.py
@@ -10,3 +10,20 @@
         print("X is greater than Y")
         else 
         print("Y is greater than X")
+
+        
+        #while loops
+con = 1
+while con < 10:
+    print (con)
+    con +=1
+
+# the result will be a count from 1-9
+
+
+
+
+#for  loop
+exampleList = [1,2,4,77,89,101]
+for eachNumber in exampleList:
+    print(eachNumber)
