d52e56f5ca39bdd465d6bb007eee317fa68afe27
Brandon <jayster@protonmail.com>
2017-04-12 22:35:49 -0500

    Create Full_lesson.py

---
+++ Full_lesson.py
@@ -0,0 +1,12 @@
+ #if statement
+    x = 51
+    y = 12
+    if x > y
+        print("X is greater than Y")
+    #out put will be X is greater than y
+    
+    #if else statements
+     if x > y
+        print("X is greater than Y")
+        else 
+        print("Y is greater than X")
