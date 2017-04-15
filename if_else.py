9be94c072ad5aa674e67a98a57e3633807904f0b
Brandon <jayster@protonmail.com>
2017-04-15 16:43:37 -0500

    Corrected the syntax

--- if_else.py
+++ if_else.py
@@ -1,14 +1,14 @@
  #if statement
-    x = 51
-    y = 12
-    if x > y
+ x = 51
+ y = 12
+    if x > y:
         print("X is greater than Y")
     #out put will be X is greater than y
     
     #if else statements
-     if x > y
+     if x > y:
         print("X is greater than Y")
-        else 
+ else: 
         print("Y is greater than X")
         
         #ok so now we will add all the loop stuff etc into one folder
