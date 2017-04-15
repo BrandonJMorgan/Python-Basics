1915713f18db3860b20e1e9b9b4eb3b37f471043
brandonjayster <jayster@protonmail.com>
2017-04-15 12:03:24 -0500

    make a simple calculator in python

---
+++ calculator.py
@@ -0,0 +1,23 @@
+class calculator:
+        def addition(x,y):
+                add = x + y
+                print(add)
+
+        def subtraction(x,y):
+                sub = x - y
+                print (sub)
+
+
+        def division(x,y):
+                div = x / y
+                print(div)
+
+
+        def multiplication(x,y):
+                mul = x * y
+                print(mul)
+        
+
+calculator.multiplication(25,10)
+
+	
