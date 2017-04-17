f1156092bcad01046b754951709a22572a68b072
Brandon <jayster@protonmail.com>
2017-04-16 21:32:05 -0500

    changed pdata to pd since pd is a standard

--- Data analysis/data.py
+++ Data analysis/data.py
@@ -1,8 +1,8 @@
-import pandas as pdata
+import pandas as pd
 
 web_data = {'Day':[1,2,3,4,5,6],
              'Visitors':[10,500,657,778,2009,1299],
              'Bounce Rate':[9,268,403,667,700,1102]}
 
-df = pdata.DataFrame(web_data)
+df = pd.DataFrame(web_data)
 print(df.head())
