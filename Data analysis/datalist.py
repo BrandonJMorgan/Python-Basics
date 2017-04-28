b10e9bdd2f3e954eb370d0f770edba459309eea8
brandonjayster <jayster@protonmail.com>
2017-04-27 19:33:47 -0500

    How to index list in python

---
+++ Data analysis/datalist.py
@@ -0,0 +1,8 @@
+#the Names variable stores data in this example it stores names then we call print to write 
+#that daat out to a file
+Names = ['Allen', 'Brian', 'Brandon', 'Austin', 'Mekenzie', 'Selena']
+#print (Names)
+#int this code below this will index names one thing to remeber is python
+#counts from 0 and up
+print ('First person is', Names[0])
+print('Second person is', Names[1])
