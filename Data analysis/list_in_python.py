1c3e06216da2dfe7881131d57823e3a2fe749dab
Brandon <jayster@protonmail.com>
2017-04-29 16:08:34 -0500

    List are important in data analysis

---
+++ Data analysis/list_in_python.py
@@ -0,0 +1,13 @@
+computers = ['dell', 'alienware', 'hp', 'lenvoe', 'surfacebook']
+#print out the list
+print('Computer list', computers)
+#add Apple computers to the list
+computers.append('Apple')
+print('Appended:', computers)
+print(len(computers))
+
+#tuples which are not used much a tuple is the same thing as a list but not as functionals
+computers = ('dell', 'alienware', 'hp', 'lenvoe', 'surfacebook')
+print('computers', computers)
+# below we are indexing this works the same in a list just as a tuple 
+print('computers:', computers[-1])
