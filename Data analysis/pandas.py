ac71a4badeb300a8f3e21a12210d6772622ba590
Brandon <jayster@protonmail.com>
2017-04-29 14:40:13 -0500

    added more data

--- Data analysis/pandas.py
+++ Data analysis/pandas.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('ggplot')
 
-web_data = {'days': [1,2,3],
-			'visitors': [43,53,77]
-			'bouncerate': [10,33,47]}
+web_data = {'days': [1,2,3,4,5],
+			'visitors': [43,53,77,100,90],
+			'bouncerate': [10,33,47,20,50]}
 
 df = pd.DataFrame(web_data)
 print(df)
+#print(df.head(2)) this will print the first 2 same with .tail()the last said numbers
