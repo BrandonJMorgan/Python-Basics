3de80a0c8aaa33e63da88002005b7924bdc75bc0
Brandon <jayster@protonmail.com>
2017-04-29 14:31:02 -0500

    Delete data.py

--- Data analysis/data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-web_data = {'Day':[1,2,3,4,5,6],
-             'Visitors':[10,500,657,778,2009,1299],
-             'Bounce Rate':[9,268,403,667,700,1102]}
-
-df = pd.DataFrame(web_data)
-print(df.head())
